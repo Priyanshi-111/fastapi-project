939fa03d2781a7fd29e33a0a0eca91cf163699f4
Priyanshi-111 <tomarpriyanshi9259@gmail.com>
2026-06-27 16:38:25 +0530

    First FastAPI CRUD project

---
+++ README.md
@@ -0,0 +1,15 @@
+# FastAPI CRUD Project
+
+## Run Project
+
+Install packages:
+
+pip install -r requirements.txt
+
+Run server:
+
+uvicorn main:app --reload
+
+Open:
+
+http://127.0.0.1:8000/docs
---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Temporary database
+users = []
+
+
+# Home route
+@app.get("/")
+def home():
+    return {
+        "message": "API is working"
+    }
+
+
+# CREATE - POST
+@app.post("/users")
+def create_user(user: dict):
+    users.append(user)
+    return {
+        "message": "User created successfully",
+        "data": user
+    }
+
+
+# READ - GET
+@app.get("/users")
+def get_users():
+    return {
+        "users": users
+    }
+
+
+# UPDATE - PUT
+@app.put("/users/{id}")
+def update_user(id: int, user: dict):
+    if id >= len(users):
+        return {
+            "message": "User not found"
+        }
+
+    users[id] = user
+
+    return {
+        "message": "User updated successfully",
+        "data": user
+    }
+
+
+# DELETE - DELETE
+@app.delete("/users/{id}")
+def delete_user(id: int):
+    if id >= len(users):
+        return {
+            "message": "User not found"
+        }
+
+    deleted_user = users.pop(id)
+
+    return {
+        "message": "User deleted successfully",
+        "data": deleted_user
+    }
